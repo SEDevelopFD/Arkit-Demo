fe4a0ea11c2ac6dd0c1c3e2ea370410a9f5f68dd
Pan <sicheng.pan.chn@gmail.com>
2018-10-25 23:08:47 +0800

    demo
    done

---
+++ Assets/manageResource.cs
@@ -0,0 +1,30 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manageResource : MonoBehaviour {
+	transfrom[] resources;
+	public bool resourceExists = false;
+	// Use this for initialization
+	void Start () {
+		//search in children for target resource
+		resources = GameObject.findobjectsinchildrenbytag
+	}
+
+	int spawn(string name){
+		//load resource from files
+		//set parent for this resource
+	}
+
+
+	int upspawn(transfrom target){
+		//delete resource from scene
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+ */

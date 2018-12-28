a46158417f4ede136f65d6cd8f1f399f3479a777
YixuanWei <yixuan_wei@qq.com>
2018-12-28 13:39:24 +0800

    networkInit

---
+++ Assets/Scripts/Network/SyncFurniture.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncFurniture : NetworkBehaviour {
+
+    [SyncVar]
+    private Quaternion rotation;
+    [SyncVar]
+    private Vector3 position;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(transform.position);
+        if(hasAuthority){
+            transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Debug.Log("system ready to cmd sync");
+            CmdSyncTransform(transform.rotation,transform.position);
+        }
+        transform.rotation = rotation;
+        transform.position = position;
+	}
+    [Command]
+    void CmdSyncTransform(Quaternion r, Vector3 p){
+        Debug.Log(GetComponent<NetworkIdentity>().netId);
+        rotation = r;
+        position = p;
+    }
+}
---
+++ Assets/Scripts/Network/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    [SerializeField]
+    private GameObject target;
+	// Use this for initialization
+	void Start () {
+        GameObject.Instantiate(target);
+	}
+	
+	
+}
